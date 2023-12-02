e8948f7db404abef1b0b183392f83abc6352e8bd
Shaddy <shadybett540@gmail.com>
2023-12-02 09:19:15 +0300

    done

--- lib/dog.py
+++ lib/dog.py
@@ -1,15 +1,24 @@
-#!/usr/bin/env python3
-
-APPROVED_BREEDS = [
-    "Mastiff",
-    "Chihuahua",
-    "Corgi",
-    "Shar Pei",
-    "Beagle",
-    "French Bulldog",
-    "Pug",
-    "Pointer"
-]
-
 class Dog:
-    pass
+    def __init__(self) -> None:
+        self._name = ''
+
+    def get_name(self):
+        print('getting name')
+        return self._name
+
+    def set_name(self, name):
+        if isinstance(name, str) and (1 <= len(name) <= 2):
+            self._name = name
+        else:
+            print('Name must be a string between 1 and 25 characters.')
+
+    name = property(get_name, set_name)
+
+# Creating an instance of the Dog class
+d = Dog()
+
+# Using the setter method to set the name
+d.name = 'hello'
+
+# Using the getter method to get the name
+print(d.name)
